3b45616bb411f59fa1c400ce018053f23b52e9cf
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-02-25 18:36:38 +0200

    add change

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,27 @@ import { useState } from "react";
 
 export default function Home() {
   const { isConnected } = useAccount();
-  const [usdt, setUsdt] = useState(1);
+  const [usdt, setUsdt] = useState(''); 
   const [trx, setTrx] = useState(0); 
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+
+  const toggleModal2 = () => {
+    setIsOpen2(prevState => !prevState);
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   const handleUsdtChange = (e) => {
-    const usdtValue = parseFloat(e.target.value); 
-    setUsdt(usdtValue);  
+    const usdtValue = parseFloat(e.target.value) || 0; 
+    setUsdt(e.target.value); 
     const exchangeRate = 4.03; 
     setTrx(usdtValue * exchangeRate);
   };
@@ -19,7 +34,6 @@ export default function Home() {
   const toggleBlock = (index) => {
     setOpenBlock(openBlock === index ? null : index);
   };
-
   return (
     <>
     <div className="bg-[#061313] w-full min-h-screen flex flex-col pt-8 items-center text-white px-6">
@@ -155,10 +169,11 @@ export default function Home() {
           <button
             className="text-gray-400 text-sm hover:text-gray-300 flex items-center gap-1"
             id="openModal2"
+            onClick={toggleModal2}
             >
-            Дополнительно{" "}
+            Дополнительно
             <svg
-              className="lucide lucide-chevron-down"
+              className={`lucide lucide-chevron-down transition-transform duration-200 ${isOpen2 ? 'rotate-180' : ''}`}
               fill="none"
               height="16"
               id="block"
@@ -174,18 +189,20 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="space-y-2" id="modal2" role="dialog">
-        <p>Получатель TRX:</p>
-        <input
-          className="w-full px-3 py-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-[#339192]"
-          defaultValue=""
-          type="text"
-        />
-        <p className="text-gray-400 text-sm">
-          Опционально. Используйте это поле если хотите чтобы кто-то другой
-          получил купленные TRX.
-        </p>
-      </div>
+      {isOpen2 && (
+        <div className="space-y-2" id="modal2" role="dialog">
+          <p>Получатель TRX:</p>
+          <input
+            className="w-full px-3 py-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-[#339192]"
+            defaultValue=""
+            type="text"
+          />
+          <p className="text-gray-400 text-sm">
+            Опционально. Используйте это поле если хотите чтобы кто-то другой
+            получил купленные TRX.
+          </p>
+        </div>
+      )}
       <div>
         <p>Цена TRX: ~ $0.248125</p>
         <p className="text-gray-400 text-sm">
@@ -211,74 +228,82 @@ export default function Home() {
           aria-controls="radix-:r3:"
           aria-expanded="false"
           aria-haspopup="dialog"
+          onClick={openModal}
           className="w-full flex justify-center"
           data-state="closed"
           type="button">
+
           <p
             className="border-b-2 text-gray-400 border-gray-400 hover:text-gray-500 hover:border-gray-500"
-            id="openModal">
+            id="openModal"
+            onClick={openModal}
+            >
             Как пользоваться сайтом?
           </p>
         </button>
-      </div>{" "}
-      <div
-        className="fixed left-[50%] top-[50%] z-50 hidden w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 bg-gray-800 text-white rounded-lg"
-        id="modal"
-        role="dialog">
-        <div className="text-center">
-          <h2 className="text-2xl font-semibold mb-4">
-            Как пользоваться сайтом?
-          </h2>
-        </div>
-        <p>
-          <span className="text-xl">Поддерживаемые кошельки</span>
-          <br />
-          <span className="text-gray-400">
-            buytrx.org работает с Trust Wallet и TronLink.
-          </span>
-        </p>
-        <p>
-          <span className="text-xl">Краткая инструкция:</span>
-          <br />
-          <span className="text-gray-400">
-            1. Откройте buytrx.org во встроенном браузере вашего кошелька.
-            <br />
-            2. Введите сколько TRX вы хотите купить.
-            <br />
-            3. Нажмите "Купить".
-            <br />
-            4. Подключите кошелек.
-            <br />
-            5. Подтвердите перевод USDT.
-          </span>
-        </p>
-        <hr />
-        <p className="text-gray-400">
-          Смотрите подробную видео инструкцию{" "}
-          <a
-            className="underline"
-            href="https://www.youtube.com/playlist?list=PLnr4C8ZUG7OHUdcr3TiLVJUdOpUCi6Z1f">
-            в этом YouTube плейлисте.
-          </a>
-        </p>
-        <button
-          className="absolute right-4 top-4 text-gray-400 hover:text-gray-500"
-          id="closeModal">
-          <svg
-            fill="none"
-            height="24"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M18 6L6 18" />
-            <path d="M6 6L18 18" />
-          </svg>
-        </button>
       </div>
+      {isOpen && (
+         <div
+         className={`fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 bg-gray-800 text-white rounded-lg ${isOpen ? 'block' : 'hidden'}`}
+         id="modal"
+         role="dialog">
+         <div className="text-center">
+           <h2 className="text-2xl font-semibold mb-4">
+             Как пользоваться сайтом?
+           </h2>
+         </div>
+         <p>
+           <span className="text-xl">Поддерживаемые кошельки</span>
+           <br />
+           <span className="text-gray-400">
+             buytrx.org работает с Trust Wallet и TronLink.
+           </span>
+         </p>
+         <p>
+           <span className="text-xl">Краткая инструкция:</span>
+           <br />
+           <span className="text-gray-400">
+             1. Откройте buytrx.org во встроенном браузере вашего кошелька.
+             <br />
+             2. Введите сколько TRX вы хотите купить.
+             <br />
+             3. Нажмите "Купить".
+             <br />
+             4. Подключите кошелек.
+             <br />
+             5. Подтвердите перевод USDT.
+           </span>
+         </p>
+         <hr />
+         <p className="text-gray-400">
+           Смотрите подробную видео инструкцию{" "}
+           <a
+             className="underline"
+             href="https://www.youtube.com/playlist?list=PLnr4C8ZUG7OHUdcr3TiLVJUdOpUCi6Z1f">
+             в этом YouTube плейлисте.
+           </a>
+         </p>
+         <button
+           className="absolute right-4 top-4 text-gray-400 hover:text-gray-500"
+           id="closeModal"
+           onClick={closeModal}
+           >
+           <svg
+             fill="none"
+             height="24"
+             stroke="currentColor"
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             strokeWidth="2"
+             viewBox="0 0 24 24"
+             width="24"
+             xmlns="http://www.w3.org/2000/svg">
+             <path d="M18 6L6 18" />
+             <path d="M6 6L18 18" />
+           </svg>
+         </button>
+       </div>
+      )}
       <div className="space-y-1 text-sm">
         <div
           className="text-gray-400 flex flex-row items-center gap-2"
